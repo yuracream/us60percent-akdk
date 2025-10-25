fc718ed969123f5147e45d0086ddf8edfc0e37b9
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-25 19:56:38 +0900

    Update keymap.c

--- qmk_firmware/keyboards/us60percent/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/us60percent/keymaps/vial/keymap.c
@@ -1,2 +1,4 @@
 #include QMK_KEYBOARD_H
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( /* ← keyboard.json の LAYOUT 個数ぶん（この基板は 67 個）を KC_NO で埋める */ )
+};
