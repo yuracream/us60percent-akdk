bfc1491ce5a047cb538157d2ea6f3e2054a7a7de
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-25 19:22:23 +0900

    Delete us60percent-akdk directory

--- us60percent-akdk/qmk_firmware/keyboards/us60percent/keymaps/vial/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xe7, 0xce, 0x8c, 0xcf, 0x62, 0x95, 0x6b, 0xbb}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
-#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
